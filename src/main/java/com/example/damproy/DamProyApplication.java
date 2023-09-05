c378c38196d892bd41e23743baa2036ed0447484
Juan <juanignaciomolet@gmail.com>
2023-09-05 15:55:23 -0300

    first commit

---
+++ src/main/java/com/example/damproy/DamProyApplication.java
@@ -0,0 +1,14 @@
+package com.example.damproy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DamProyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DamProyApplication.class, args);
+		System.out.println("Hola mundo!");
+	}
+
+}
